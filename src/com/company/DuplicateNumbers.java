0e7913a2cb4193dc14c789da5dfc6d813cd44d76
Alex <alex.assali@gmail.com>
2022-08-29 14:53:41 -0400

    duplicate number

---
+++ src/com/company/DuplicateNumbers.java
@@ -0,0 +1,22 @@
+package com.company;
+
+public class DuplicateNumbers {
+    public static void main(String[] args) {
+     int arr[] = new int[]{1,2,3,2,4};
+        System.out.println("The duplicate Number is: " + findDuplicate(arr) );
+    }
+
+    private static int findDuplicate(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            int index = Math.abs(arr[i]) - 1;
+            System.out.println("index: " + index);
+            System.out.println(" arr index " + arr[index]);
+            if(arr[index] < 0) {
+                return Math.abs(arr[i]);
+            }
+            arr[index] = -arr[index];
+        }
+        return -1;
+    }
+
+}
